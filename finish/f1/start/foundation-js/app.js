145ef4d68e0bb1b41cd24602af95c1e46354fe37
syntaxSizer <kakuma4never@gmail.com>
2016-04-18 20:06:24 +0300

    add memebers section

--- finish/f1/start/foundation-js/app.js
+++ finish/f1/start/foundation-js/app.js
@@ -1,1 +1,32 @@
 $(document).foundation()
+    (function () {
+
+        function init() {
+            var speed = 250,
+                easing = mina.easeinout;
+
+		[].slice.call(document.querySelectorAll('#grid > a')).forEach(function (el) {
+                var s = Snap(el.querySelector('svg')),
+                    path = s.select('path'),
+                    pathConfig = {
+                        from: path.attr('d'),
+                        to: el.getAttribute('data-path-hover')
+                    };
+
+                el.addEventListener('mouseenter', function () {
+                    path.animate({
+                        'path': pathConfig.to
+                    }, speed, easing);
+                });
+
+                el.addEventListener('mouseleave', function () {
+                    path.animate({
+                        'path': pathConfig.from
+                    }, speed, easing);
+                });
+            });
+        }
+
+        init();
+
+    })();
