c02fdd4e399710d3699a94a9728cad8bed7fd40d
syntaxSizer <kakuma4never@gmail.com>
2016-04-22 13:43:42 +0300

    change sections to divs

--- finish/f1/start/foundation-js/app.js
+++ finish/f1/start/foundation-js/app.js
@@ -1,4 +1,5 @@
 $(document).foundation()
+    //form animation
     (function () {
 
         function init() {
@@ -30,3 +31,19 @@ $(document).foundation()
         init();
 
     })();
+
+// //scroll effect on navigating to other pages
+
+$(document).foundation()
+$('a[href^="#"]').on('click', function (event) {
+
+    var target = $($(this).attr('href'));
+
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
