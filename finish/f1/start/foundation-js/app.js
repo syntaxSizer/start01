d62b5573a281a57aec905585afbcba6911a23e1f
syntaxSizer <kakuma4never@gmail.com>
2016-04-30 05:08:33 +0300

    add scroll effect

--- finish/f1/start/foundation-js/app.js
+++ finish/f1/start/foundation-js/app.js
@@ -34,16 +34,17 @@ $(document).foundation()
 
 // //scroll effect on navigating to other pages
 
-$(document).foundation()
-$('a[href^="#"]').on('click', function (event) {
-
-    var target = $($(this).attr('href'));
-
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
