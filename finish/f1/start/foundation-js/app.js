b142ce199735a2d76a4eb6bdfcae3830c4b57754
syntaxSizer <kakuma4never@gmail.com>
2016-05-17 18:12:47 +0300

    moving js to extrnal js script file

--- finish/f1/start/foundation-js/app.js
+++ finish/f1/start/foundation-js/app.js
@@ -48,3 +48,84 @@ $(function () {
         }
     });
 });
+
+
+
+
+(function () {
+            [].slice.call(document.querySelectorAll('.menu')).forEach(function (menu) {
+        var menuItems = menu.querySelectorAll('.menu__link'),
+            setCurrent = function (ev) {
+                //                        ev.preventDefault();
+
+                var item = ev.target.parentNode; // li
+
+                // return if already current
+                if (classie.has(item, 'menu__item--current')) {
+                    return false;
+                }
+                // remove current
+                classie.remove(menu.querySelector('.menu__item--current'), 'menu__item--current');
+                // set current
+                classie.add(item, 'menu__item--current');
+            };
+
+                [].slice.call(menuItems).forEach(function (el) {
+            el.addEventListener('click', setCurrent);
+        });
+    });
+
+            [].slice.call(document.querySelectorAll('.link-copy')).forEach(function (link) {
+        link.setAttribute('data-clipboard-text', location.protocol + '//' + location.host + location.pathname + '#' + link.parentNode.id);
+        new Clipboard(link);
+        link.addEventListener('click', function () {
+            classie.add(link, 'link-copy--animate');
+            setTimeout(function () {
+                classie.remove(link, 'link-copy--animate');
+            }, 300);
+        });
+    });
+})(window);
+
+< /script> < script src = "form-js/classie.js " > < /script> < script > (function () {
+    // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+    if (!String.prototype.trim) {
+        (function () {
+            // Make sure we trim BOM and NBSP
+            var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            String.prototype.trim = function () {
+                return this.replace(rtrim, '');
+            };
+        })();
+    }
+
+            [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
+        // in case the input is already filled..
+        if (inputEl.value.trim() !== '') {
+            classie.add(inputEl.parentNode, 'input--filled');
+        }
+
+        // events:
+        inputEl.addEventListener('focus', onInputFocus);
+        inputEl.addEventListener('blur', onInputBlur);
+    });
+
+    function onInputFocus(ev) {
+        classie.add(ev.target.parentNode, 'input--filled');
+    }
+
+    function onInputBlur(ev) {
+        if (ev.target.value.trim() === '') {
+            classie.remove(ev.target.parentNode, 'input--filled');
+        }
+    }
+})();
+
+< /script> < script > (function () {
+    var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+    if (isSafari) {
+        document.getElementById('support-note').style.display = 'block';
+    }
+})();
+
+< /script>
